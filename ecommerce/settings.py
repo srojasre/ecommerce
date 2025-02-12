e51c2ac0eb9fd15f1306605399ac0f3e5b29161e
srojasre <s.rojasr2@uniandes.edu.co>
2025-02-12 14:10:16 -0500

    Swamsports config init

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 
+SECRET_KEY = 'favt-0=li&2csex_^sfxptebit7nz86ln3^vn@g2e*$p!gjr#v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
